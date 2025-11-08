971db662432d61000816c5c77bca9ddaa031a2ea
katya-voronova <katya@example.com>
2025-11-08 21:31:55 +0300

    update

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,4 +1,4 @@
-import Flask
+from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from settings import Config
@@ -8,4 +8,4 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from . import api_views, error_handlers, yacutfrom
+from . import api_views, error_handlers, yacut
